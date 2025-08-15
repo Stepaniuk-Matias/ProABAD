303fc5dcd0c2ec2a441ff079ae2bb8a33deead66
mati_stepa <mstepaniuk@miguelabad.com.ar>
2025-08-15 08:30:48 -0300

    Se crea menu bar con opciones premilimares (sin funcionalidad)

--- src/ui/main_window.py
+++ src/ui/main_window.py
@@ -14,4 +14,28 @@ class MainWindow(QMainWindow):
         self.setMinimumSize(QSize(800, 600))
 
         menu_bar = self.menuBar()
+
         familias_y_atributos = menu_bar.addMenu("Familias y Atributos")
+
+        accion_crear_familia = QAction("Crear", self) 
+        familias_y_atributos.addAction(accion_crear_familia)
+
+        accion_ver_familias = QAction("Ver y editar", self)
+        familias_y_atributos.addAction(accion_ver_familias)
+
+        productos = menu_bar.addMenu("Productos")
+        accion_crear_producto = QAction("Crear", self)
+        productos.addAction(accion_crear_producto)
+        accion_ver_productos = QAction("Ver y editar", self)
+        productos.addAction(accion_ver_productos)
+
+        procesos = menu_bar.addMenu("Procesos")
+
+        accion_crear_proceso = QAction("Crear", self)
+        procesos.addAction(accion_crear_proceso)
+
+        accion_ver_procesos = QAction("Ver y editar", self)
+        procesos.addAction(accion_ver_procesos)
+
+        accion_comparar_procesos = QAction("Comparar procesos", self)
+        procesos.addAction(accion_comparar_procesos)
