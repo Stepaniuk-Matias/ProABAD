a3f8db7a314991e55483726fae4780e43180878b
mati_stepa <mstepaniuk@miguelabad.com.ar>
2025-09-18 12:46:46 -0300

    Eliminacion de codigo viejo y correccion de error en query select_familys_atributes

--- src/controllers/main_window_controller.py
+++ src/controllers/main_window_controller.py
@@ -5,27 +5,13 @@ from views.Ui_main_window_view import Ui_Form
 
 from controllers.family_atributes_window_controller import FamilyAtributeWindow
 
-from views.tree_view import TreeView
-from models.tree_model import TreeModel
-from utils.tree_item import TreeItem
 
 class MainWindow(QWidget, Ui_Form):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
 
-        # Se crea root item y modelo para el árbol
-
-        self.root_item = TreeItem(["Familia","Atributo","Tipo de dato"]).cargar_datos()
-        self.model = TreeModel(self.root_item)
-
-        # Se crea tree view y se vincula con el modelo
-
-        self.tree_view = TreeView()
-        self.tree_view.setModel(model=self.model)
-
         self.familys_button.clicked.connect(self.open_family_window)
-        self.toolButton.clicked.connect(self.tree_view.show)
 
     def open_family_window(self):
         self.w = FamilyAtributeWindow()
